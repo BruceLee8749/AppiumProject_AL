72bc119a0b554e8bcf6394815997e7d23a1d0f0b
BruceLee8749 <2803780038@qq/com>
2021-05-13 02:31:30 +0800

    '0513

--- scripts/test_clear_cache.py
+++ scripts/test_clear_cache.py
@@ -19,7 +19,7 @@ class TestClearCache:
         # 点击设置按钮
         self.page.me.click_setting()
         # 进入 设置界面 查找并点击清理缓存
-        self.page.setting.clear_cache_button()
+        self.page.setting.click_cache()
         # 查找是否 含有指定toast
         assert self.page.setting.is_toast_exist('清理成功')
 
